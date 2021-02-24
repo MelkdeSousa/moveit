af2181c0fe0c4e8d0ec004abf0e0dfccf186234f
MelkdeSousa <melk.sousap@gmail.com>
2021-02-23 23:36:51 -0300

    lesson 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
+import CompletedChallenges from '../components/CompletedChallenges'
+import Countdown from '../components/Countdown'
 import ExperienceBar from '../components/ExperienceBar'
+import Profile from '../components/Profile'
+
+import styles from '../styles/pages/Home.module.css'
 
 const Home = () => (
-  <div className='container'>
+  <div className={styles.container}>
     <ExperienceBar />
+
+    <section>
+      <div>
+        <Profile />
+        <CompletedChallenges />
+        <Countdown />
+      </div>
+      <div></div>
+    </section>
   </div>
 )
 
